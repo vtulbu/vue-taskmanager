bc041f256cde10371f3fdfb9f6bec06ddbabe68e
Vadim Tulbu <vadimtulbu@Vadims-MacBook-Pro.local>
2023-03-15 19:32:02 +0200

    cleare unused files and code

---
+++ src/components/HeaderDashboard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  boardName: string
+}>()
+</script>
+
+<template>
+  <header>
+    <p class="green">{{ boardName }}</p>
+  </header>
+</template>
+
+<style scoped></style>
