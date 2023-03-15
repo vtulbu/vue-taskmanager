243445e638449a5639a669a5d201cbac7e7903ce
Vadim Tulbu <vadimtulbu@Vadims-MacBook-Pro.local>
2023-03-15 19:31:13 +0200

    install prime and config themes

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,34 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import HeaderDashboard from './components/HeaderDashboard.vue'
+import Sidebar from './components/SideBar.vue'
+import Button from 'primevue/button'
+
+const toggleTheme = ref(localStorage.getItem('theme') === 'dark')
+
+const changeTheme = () => {
+  console.log('theme', toggleTheme)
+  if (toggleTheme.value) {
+    document.getElementById('theme-link')?.setAttribute('href', '/themes/dark.css')
+  } else {
+    document.getElementById('theme-link')?.setAttribute('href', '/themes/light.css')
+  }
+  toggleTheme.value = !toggleTheme.value
+  localStorage.setItem('theme', toggleTheme.value ? 'dark' : 'light')
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="dashboard-app">
+    <HeaderDashboard boardName="test" />
+    <Sidebar />
+    <Button icon="pi pi-arrow-right" @click="changeTheme">Change</Button>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.dashboard-app {
+  height: 100vh;
+  width: 100vw;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,28 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createHead } from '@vueuse/head'
+import PrimeVue from 'primevue/config'
 
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import 'normalize.css'
 
 const app = createApp(App)
 
+const head = createHead({
+  link: [
+    {
+      id: 'theme-link',
+      rel: 'stylesheet',
+      href: '/themes/light.css'
+    }
+  ]
+})
+
 app.use(createPinia())
 app.use(router)
+app.use(head)
+app.use(PrimeVue, { ripple: true })
 
 app.mount('#app')
