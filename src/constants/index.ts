3dad9c9a45bb89ac996eba228df391be9fe368bd
Tulbu Vadim <tulbu.vadim@gmail.com>
2023-04-03 12:27:41 +0300

    Delete hyphens from id in mock data

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,17 +6,17 @@ export const screenSizes = {
 } as const
 
 export const boardsMock = Array.from({ length: 10 }, (_, i) => ({
-  id: `board-${i}`,
+  id: `board${i}`,
   label: `Board ${i}`,
   columns: Array.from({ length: 6 }, (_, j) => ({
-    id: `column-${i}-${j}`,
+    id: `column${i}${j}`,
     label: `Column ${j}`,
     tasks: Array.from({ length: 6 }, (_, k) => ({
-      id: `task-${i}-${j}-${k}`,
+      id: `task${i}${j}${k}`,
       title: `Task ${k}`,
       description: `Description ${k}: We know what we're planning to build for version one. Now we need to finalise the first pricing model we'll use. Keep iterating the subtasks until we have a coherent proposition.`,
       subtasks: Array.from({ length: 5 }, (_, l) => ({
-        id: `subtask-${i}-${j}-${k}-${l}`,
+        id: `subtask${i}${j}${k}${l}`,
         label: `Subtask ${l}`,
         isDone: l % 2 === 0
       }))
